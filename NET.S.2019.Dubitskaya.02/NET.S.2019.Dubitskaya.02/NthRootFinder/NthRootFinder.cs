23ebc6dd7e4e8b0edb6c3ea34a654c79cfaa77b4
Karalina Dubitskaya <dubitskaya.karalina@gmail.com>
2019-03-24 11:55:40 +0300

    Add README.md to Day #2

--- NET.S.2019.Dubitskaya.02/NET.S.2019.Dubitskaya.02/NthRootFinder/NthRootFinder.cs
+++ NET.S.2019.Dubitskaya.02/NET.S.2019.Dubitskaya.02/NthRootFinder/NthRootFinder.cs
@@ -15,22 +15,22 @@ namespace NthRootFinding
         /// <param name="rootPower">A root to be found.</param>
         /// <param name="eps">Calculation precision.</param>
         /// <returns>Nth root of the number.</returns>
-        /// <exception cref="ArgumentException">Thrown when the epsilon value is negative.</exception>
-        /// <exception cref="ArgumentException">Thrown when the root power is 0.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the epsilon value is negative.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the root power isn't positive.</exception>
         /// <exception cref="ArgumentException">Thrown when the root power is even and the number is negative.</exception>
         /// <example> FindNthRoot(1, 5, 0.001) -> 1 </example>
         /// <example> FindNthRoot(0.04100625, 4, 0.0001) -> 0.45 </example>
         /// <example> FindNthRoot(-0.008, 3, 0.1) -> -0.2 </example>
-        public static double FindNthRoot(double number, double rootPower, double eps)
+        public static double FindNthRoot(double number, long rootPower, double eps)
         {
             if (eps < 0)
             {
-                throw new ArgumentException("The epsilon value cannot be negative.");
+                throw new ArgumentOutOfRangeException("The epsilon value cannot be negative.");
             }
-
-            if (rootPower == 0)
+            
+            if (rootPower <= 0)
             {
-                throw new ArgumentException("The root power value cannot be 0.");
+                throw new ArgumentOutOfRangeException("The root power value should be positive.");
             }
 
             if (number < 0 && rootPower % 2 == 0)
@@ -50,19 +50,14 @@ namespace NthRootFinding
                 return number;
             }
 
-            if (rootPower < 0)
-            {
-                rootPower = Math.Abs(rootPower);
-                number = 1 / number;
-            }
-
-            double x0 = number / rootPower;
-            double x1 = (1 / rootPower) * (((rootPower - 1) * x0) + (number / Math.Pow(x0, rootPower - 1)));
+            double power = rootPower;
+            double x0 = number / power;
+            double x1 = (1 / power) * (((power - 1) * x0) + (number / Math.Pow(x0, power - 1)));
 
             while (Math.Abs(x1 - x0) >= eps)
             {
                 x0 = x1;
-                x1 = (1 / rootPower) * (((rootPower - 1) * x0) + (number / Math.Pow(x0, rootPower - 1)));
+                x1 = (1 / power) * (((power - 1) * x0) + (number / Math.Pow(x0, power - 1)));
             }
 
             return x1;
