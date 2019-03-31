39b06e4983b6e5fa80a1c8f562c89e39655a12df
Karalina Dubitskaya <dubitskaya.karalina@gmail.com>
2019-04-01 01:50:44 +0300

    Add JaggedArraySorter task to Day #5

---
+++ NET.S.2019.Dubitskaya.05/JaggedArraysSorting.Tests/JaggedArraysSorterTests.cs
@@ -0,0 +1,184 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArraysSorting.Tests
+{
+    [TestFixture]
+    public class JaggedArraysSorterTests
+    {
+        private readonly int[][] testArray =
+            {
+            new int[] { 1, 2 },
+            new int[] { 9, 7, 4, 6 },
+            new int[] { 8, 9, 10 },
+            new int[] { 3, 4, 5, 6, 7 },
+            new int[] { 1 }
+        };
+
+        [Test]
+        public void SortBySumUp_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 1 };
+            expected[1] = new int[2] { 1, 2 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 9, 7, 4, 6 };
+            expected[4] = new int[3] { 8, 9, 10 };
+
+            JaggedArraySorter.SortBySumUp(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortBySumDown_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 8, 9, 10 };
+            expected[1] = new int[] { 9, 7, 4, 6 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 1, 2 };
+            expected[4] = new int[] { 1 };
+
+            JaggedArraySorter.SortBySumDown(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null)]
+        public void SortBySum_NullAsParam_ArgumentNullExeption(int[][] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortBySumUp(array));
+        }
+
+        [Test]
+        public void SortBySum_IncorrectParam_ArgumentNullException()
+        {
+            int[][] array = new int[5][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[] { 9, 7, 4, 6 };
+            array[2] = new int[3] { 8, 9, 10 };
+            array[3] = new int[] { 3, 4, 5, 6, 7 };
+
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortBySumUp(array));
+        }
+
+        [Test]
+        public void SortByMinUp_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 1, 2 };
+            expected[1] = new int[] { 1 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 9, 7, 4, 6 };
+            expected[4] = new int[3] { 8, 9, 10 };
+
+            JaggedArraySorter.SortByMinUp(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMinDown_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 8, 9, 10 };
+            expected[1] = new int[] { 9, 7, 4, 6 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 1, 2 };
+            expected[4] = new int[] { 1 };
+
+            JaggedArraySorter.SortByMinDown(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null)]
+        public void SortByMin_NullAsParam_ArgumentNullExeption(int[][] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortByMinUp(array));
+        }
+
+        [Test]
+        public void SortByMin_IncorrectParam()
+        {
+            int[][] array = new int[5][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[] { 9, 7, 4, 6 };
+            array[2] = new int[3] { 8, 9, 10 };
+            array[3] = new int[] { 3, 4, 5, 6, 7 };
+
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortByMinUp(array));
+        }
+
+        [Test]
+        public void SortByMaxUp_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 1 };
+            expected[1] = new int[] { 1, 2 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 9, 7, 4, 6 };
+            expected[4] = new int[3] { 8, 9, 10 };
+
+            JaggedArraySorter.SortByMaxUp(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMaxDown_CorrectParam()
+        {
+            int[][] actual = new int[testArray.Length][];
+            testArray.CopyTo(actual, 0);
+
+            int[][] expected = new int[5][];
+            expected[0] = new int[] { 8, 9, 10 };
+            expected[1] = new int[] { 9, 7, 4, 6 };
+            expected[2] = new int[] { 3, 4, 5, 6, 7 };
+            expected[3] = new int[] { 1, 2 };
+            expected[4] = new int[] { 1 };
+
+            JaggedArraySorter.SortByMaxDown(actual);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null)]
+        public void SortByMax_NullAsParam_ArgumentNullExeption(int[][] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortByMaxUp(array));
+        }
+
+        [Test]
+        public void SortByMax_IncorrectParam()
+        {
+            int[][] array = new int[5][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[] { 9, 7, 4, 6 };
+            array[2] = new int[3] { 8, 9, 10 };
+            array[3] = new int[] { 3, 4, 5, 6, 7 };
+
+            Assert.Throws<ArgumentNullException>(() => JaggedArraySorter.SortByMaxUp(array));
+        }
+    }
+}
---
+++ NET.S.2019.Dubitskaya.05/JaggedArraysSorting/JaggedArraySorter.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArraysSorting
+{
+    /// <summary>
+    /// Provides methods that sort jagged arrays.
+    /// </summary>
+    public class JaggedArraySorter
+    {
+        /// <summary>
+        /// Sorts a jugged array rows by sum of elements in ascending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortBySumUp(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Sum() > arr2.Sum());
+
+        /// <summary>
+        /// Sorts a jugged array rows by sum of elements in descending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortBySumDown(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Sum() < arr2.Sum());
+
+        /// <summary>
+        /// Sorts a jugged array rows by min element in ascending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortByMinUp(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Min() > arr2.Min());
+
+        /// <summary>
+        /// Sorts a jugged array rows by min element in descending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortByMinDown(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Min() < arr2.Min());
+
+        /// <summary>
+        /// Sorts a jugged array rows by max element in ascending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortByMaxUp(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Max() > arr2.Max());
+
+        /// <summary>
+        /// Sorts a jugged array rows by max element in descending order.
+        /// </summary>
+        /// <param name="array">A jagged array to be sorted</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        public static void SortByMaxDown(int[][] jaggedArray)
+            => Sort(jaggedArray, (arr1, arr2) => arr1.Max() < arr2.Max());
+
+        /// <summary>
+        /// Sorts a jugged array by rows according to specified comparator.
+        /// </summary>
+        /// <param name="array">The array to be sorted</param>
+        /// <param name="NeedToBeSwapped">Function to compare rows.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        private static void Sort(int[][] array, Func<int[], int[], bool> needToBeSwapped)
+        {
+            CheckArray(array);
+
+            for (int j = 0; j < array.GetLength(0); j++)
+            {
+                for (int i = 0; i < array.GetLength(0) - j - 1; i++)
+                {
+                    if (needToBeSwapped(array[i], array[i + 1]))
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Swap two arrays.
+        /// </summary>
+        /// <param name="arr1">An integer array to be swapped.</param>
+        /// <param name="arr2">An integer array to be swapped.</param>
+        private static void Swap(ref int[] arr1, ref int[] arr2)
+        {
+            int[] tempArr = arr1;
+            arr1 = arr2;
+            arr2 = tempArr;
+        }
+
+        /// <summary>
+        /// Checks if the array is valid.
+        /// </summary>
+        /// <param name="arr">The array to be checked.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the jagged array or one of it's internal arrays is null.</exception>
+        /// <exception cref="ArgumentException>">Thrown when the jagged array's or one of it's internal arrays' length is 0.</exception>
+        private static void CheckArray(int[][] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (arr.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                if (arr[i].Length == 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
