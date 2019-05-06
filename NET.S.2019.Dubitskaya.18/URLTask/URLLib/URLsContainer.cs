c1cd14504145bbd051b942c152c4fdc11a79a710
Karalina Dubitskaya <dubitskaya.karalina@gmail.com>
2019-05-06 07:30:55 +0300

    Add Day #18

---
+++ NET.S.2019.Dubitskaya.18/URLTask/URLLib/URLsContainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace URLLib
+{
+    /// <summary>
+    /// Represents urls's container abstraction and provides it's methods
+    /// </summary>
+    public class URLsContainer
+    {
+        private string[] urls;
+
+        public string[] URLs
+        {
+            get
+            {
+                return urls;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the URLsContainer class.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns>instance</returns>
+        public URLsContainer(string filePath)
+        {
+            if (filePath == null)
+                throw new ArgumentException("Invalid path");
+            SetUrls(filePath);
+        }
+
+        /// <summary>
+        /// Reades all lines from file to string array
+        /// </summary>
+        /// <param name="filePath"></param>
+        private void SetUrls(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new ArgumentException("Incorrect file path");
+
+            urls = File.ReadAllLines(filePath);
+        }
+    }
+}
